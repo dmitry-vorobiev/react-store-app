c60cfbc6370a74ca3c40305c6e3cb6a28fffcea0
Dmitry Vorobiev <mustcheat@outlook.com>
2019-02-22 23:50:24 +0300

    auth reducer

---
+++ src/features/auth/reducers/auth.reducer.ts
@@ -0,0 +1,32 @@
+import {createSymbiote, Symbiote} from 'redux-symbiote';
+import {AnyAction, Reducer} from "redux";
+
+export interface AuthState {
+    loggedIn: boolean;
+}
+
+interface Symbiotes {
+    logIn: Symbiote<AuthState>;
+    logOut: Symbiote<AuthState>;
+}
+
+const initialState: AuthState = {
+    loggedIn: false
+};
+
+const symbiotes = {
+    logIn: (state: AuthState) => ({
+        loggedIn: true
+    }),
+    logOut: (state: AuthState) => ({
+        loggedIn: false
+    })
+};
+
+const {actions, reducer} = createSymbiote<AuthState, Symbiotes>(
+    initialState,
+    symbiotes,
+    'auth'
+);
+
+export default reducer as Reducer<AuthState, AnyAction>;
--- src/store/root.reducer.ts
+++ src/store/root.reducer.ts
@@ -1,11 +1,12 @@
 import {AnyAction, combineReducers, Reducer} from 'redux';
+import auth, {AuthState} from '../features/auth/reducers/auth.reducer';
 
-interface AppState {
-
+export interface AppState {
+    auth: AuthState;
 }
 
 const appState: Reducer<AppState, AnyAction> = combineReducers({
+    auth,
 });
 
-export const rootReducer: Reducer<AppState, AnyAction> = (state, action) =>
-    appState(state, action);
+export const rootReducer: Reducer<AppState, AnyAction> = (state, action) => appState(state, action);
